835657cf9a5aefac8d160d7849d79a9eacedd1e6
Addisalem_Seifu_Beidework <addisalemseifu@gmail.com>
2021-10-26 11:26:48 +0300

    Add app.js with dinamic ellements

---
+++ app.js
@@ -0,0 +1,6 @@
+var object = {};
+        var bookArray = [];
+        const book_input = document.querySelector('#txt');
+        const book_input_one = document.querySelector('#txtOne');
+        const add_btn = document.querySelector('.add');
+        const book_list = document.querySelector('.book-list')
