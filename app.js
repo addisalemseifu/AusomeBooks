cd6f40149ae10cd00064a7cbe5a4115367e8da90
Addisalem_Seifu_Beidework <addisalemseifu@gmail.com>
2021-10-27 17:32:08 +0300

    Fix disapearing ellements on page reload

--- app.js
+++ app.js
@@ -1,44 +1,68 @@
 let object = {};
-const bookArray = [];
 const bookInput = document.querySelector('#txt');
 const bookInputOne = document.querySelector('#txtOne');
 const addBtn = document.querySelector('.add');
 const bookList = document.querySelector('.book-list');
-const id = null;
+let bookArray = JSON.parse(localStorage.getItem('bookArray'));
+if(bookArray==null){
+    bookArray = [];
+}
 
 addBtn.addEventListener('click', (e) => {
-  object = {};
+
   if (bookInput.value !== '') {
     e.preventDefault();
+
     // create li
-    const MyBooks = document.createElement('ul');
-    const MyList = document.createElement('li');
-    const MyListTwo = document.createElement('li');
-    const Mybutton = document.createElement('button');
-    Mybutton.classList = 'remove_btn';
-    Mybutton.setAttribute('id', Math.random().toString(16).slice(2));
-    Mybutton.setAttribute('onclick', `${'remoteAt(this.id)'}`);
-    MyList.innerHTML = bookInput.value;
-    MyListTwo.innerHTML = bookInputOne.value;
-    Mybutton.innerHTML = 'Remove';
-    MyBooks.appendChild(MyList);
-    MyBooks.appendChild(MyListTwo);
-    MyBooks.appendChild(Mybutton);
-    bookList.appendChild(MyBooks);
     // var input = bookInput.value;
-    object[bookInput.value] = bookInputOne.value;
+    let object = {id:Math.random().toString(16).slice(2),booksName:bookInput.value,booksAu:bookInputOne.value};
     bookArray.push(object);
-    localStorage.setItem('bookArray', JSON.stringify(bookArray));
-    console.log(object);
-    console.log(bookArray);
-    bookInput.value = '';
-    bookInputOne.value = '';
+    display();
+
+  
   }
 });
+
 function remoteAt(id) {
   const element = document.getElementById(id);
   const index = bookArray.findIndex((prop) => prop.id === id);
   bookArray.splice(index, 1);
   element.parentElement.style.display = 'none';
+  display();
+
 }
-remoteAt(id);
+
+function display(){
+    bookList.innerHTML= "";
+    bookArray.forEach(element => {
+   
+
+        const MyBooks = document.createElement('ul');
+        const MyList = document.createElement('li');
+        const MyListTwo = document.createElement('li');
+        const Mybutton = document.createElement('button');
+        Mybutton.classList = 'remove_btn';
+        Mybutton.setAttribute('id',element.id);
+        Mybutton.setAttribute('onclick', `${'remoteAt(this.id)'}`);
+        MyList.innerHTML = element.booksName;
+        MyListTwo.innerHTML = element.booksAu;
+        Mybutton.innerHTML = 'Remove';
+        MyBooks.appendChild(MyList);
+        MyBooks.appendChild(MyListTwo);
+        MyBooks.appendChild(Mybutton);
+        bookList.appendChild(MyBooks);
+        bookInput.value = '';
+        bookInputOne.value = '';  
+        
+    });
+    localStorage.clear();
+    localStorage.setItem('bookArray', JSON.stringify(bookArray));
+
+
+}
+window.addEventListener('load', () => {
+ 
+ 
+    display();
+
+});
