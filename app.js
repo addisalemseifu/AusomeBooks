a8cb896bb8ab74545961c9d74e3c5e40e4ac816c
direakanbi <83509786+direakanbi@users.noreply.github.com>
2021-10-30 14:32:38 +0100

    Fixed navigation errors as pointed out by the reviewer

--- app.js
+++ app.js
@@ -76,3 +76,39 @@ for (let i = 0; i < dateTime.length; i += 1) {
   dateTime[i].innerHTML = now.toString();
 }
 dateTime.innerHTML = now.toString();
+
+const list = document.querySelector('#list');
+const addNew = document.querySelector('#add');
+const Contact = document.querySelector('#cont_txt');
+const listPage = document.querySelector('.list_page');
+const addNewPage = document.querySelector('.new_list');
+const ContactPage = document.querySelector('.contact_page');
+list.addEventListener('click', () => {
+  listPage.setAttribute('style', 'display: flex');
+  addNewPage.setAttribute('style', 'display: none');
+  ContactPage.setAttribute('style', 'display: none');
+});
+addNew.addEventListener('click', () => {
+  addNewPage.setAttribute('style', 'display: flex');
+  listPage.setAttribute('style', 'display: none');
+  ContactPage.setAttribute('style', 'display: none');
+});
+Contact.addEventListener('click', () => {
+  ContactPage.setAttribute('style', 'display: flex');
+  addNewPage.setAttribute('style', 'display: none');
+  listPage.setAttribute('style', 'display: none');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
